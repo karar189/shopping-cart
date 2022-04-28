70e09f932045393b36f2b7e630b942e6450dab5c
swetakarar <kararsweta@gmail.com>
2022-04-28 23:39:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [nums, setNums] = useState(0);
+
+  useEffect(() => {
+    alert("I got clicked");
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <button
+        className="btn"
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        Click me {num}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
